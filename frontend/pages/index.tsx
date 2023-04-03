66367f1c7bdaa4d6e1cfeb47cd4b50730b5f37e7
benchanjamin <47726262+benchanjamin@users.noreply.github.com>
2023-04-03 12:10:56 -0400

    make landing page responsive

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -12,11 +12,11 @@ export default function Home() {
             <header>
                 <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
                     <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-                        <Link href="/" className="flex items-center" to={}>
+                        <Link href="/" className="flex items-center">
                             <Image src="/Tigerbook.png"
                                    alt="Tigerbook Logo" width={149} height={40}/>
                         </Link>
-                        <div className="flex items-center lg:order-2">
+                        <div className="flex items-center max-[368px]:justify-between max-[368px]:flex-grow lg:order-2">
                             <Link href="#"
                                   className="text-white bg-primary-400 hover:bg-primary-500 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">Log
                                 in with CAS</Link>
