6f1991003228278bd4fd97051411ca294b4c838b
benchanjamin <47726262+benchanjamin@users.noreply.github.com>
2023-04-03 10:56:00 -0400

    remove scroll-smooth

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -4,7 +4,7 @@ import Script from "next/script";
 
 export default function Document() {
     return (
-        <Html lang="en" class="scroll-smooth">
+        <Html lang="en">
             <Head>
                 <link rel="icon" href="/favicon.ico"/>
                 <Script src="https://unpkg.com/flowbite@1.4.7/dist/flowbite.js" strategy="beforeInteractive"></Script>
