3d166f5fc60965f845f3ee1535b0e9bc9e26e97c
benchanjamin <47726262+benchanjamin@users.noreply.github.com>
2023-04-07 06:37:23 -0400

    docker ready pt 2

--- backend/tigerbook/settings.py
+++ backend/tigerbook/settings.py
@@ -135,7 +135,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+STATIC_URL = '/django_static/'
+STATIC_ROOT = BASE_DIR / 'django_static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
