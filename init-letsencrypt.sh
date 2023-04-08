2701a42f1f61669235feab3209c40692e26a51da
benchanjamin <47726262+benchanjamin@users.noreply.github.com>
2023-04-08 02:22:55 -0400

    uupdate nginx with docker-compose.yml

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(tiger-book.io www.tigerbook.io)
+domains=(tiger-book.io www.tiger-book.io)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="it.admin@tigerapps.org" # Adding a valid address is strongly recommended
