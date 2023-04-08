c6cf9535b783ff4bdaf4b04b96e53c476a6705c0
benchanjamin <47726262+benchanjamin@users.noreply.github.com>
2023-04-08 07:14:47 -0400

    add www.tiger-book.com domain

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,11 +5,11 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(tiger-book.com)
+domains=(tiger-book.com www.tiger-book.com)
 rsa_key_size=4096
 data_path="./docker/nginx/certbot"
-email="" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+email="it.admin@tigerapps.org" # Adding a valid address is strongly recommended
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
