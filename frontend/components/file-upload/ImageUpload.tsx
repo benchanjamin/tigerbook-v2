afd1bf5a4c5915ed9ef48e3c01dc096980c0b9c2
benchanjamin <47726262+benchanjamin@users.noreply.github.com>
2023-04-20 14:16:45 -0400

    center text on file upload on setup two

--- frontend/components/file-upload/ImageUpload.tsx
+++ frontend/components/file-upload/ImageUpload.tsx
@@ -47,10 +47,10 @@ function ImageUpload({data, files, setFiles}) {
                         <label htmlFor="dropzone-file"
                                className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
                             <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                                <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span
+                                <p className="mb-2 text-sm text-gray-500 dark:text-gray-400 text-center"><span
                                     className="font-semibold">Click to upload a profile picture </span>
                                 </p>
-                                <p className="text-xs text-gray-500 dark:text-gray-400">Upload an image
+                                <p className="text-xs text-gray-500 dark:text-gray-400 text-center">Upload an image
                                     type file (preferably cropped to a square image)</p>
                             </div>
                             <div {...getRootProps({className: 'dropzone'})}>
