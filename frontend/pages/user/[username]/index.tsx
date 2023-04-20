70f4eb9ce08822fff114fbabdfa324004a916645
benchanjamin <47726262+benchanjamin@users.noreply.github.com>
2023-04-20 11:40:34 -0400

    make hometowns and certificates optionally

--- frontend/pages/user/[username]/index.tsx
+++ frontend/pages/user/[username]/index.tsx
@@ -85,45 +85,55 @@ const Index: React.FC<Props> = ({userData, headerData}) => {
                                        className=" rounded-2xl"
                                 />
                             </div>
-                            <h4 className="text-primary-500 text-left mt-10 font-bold tracking-wide">
-                                CERTIFICATES
-                            </h4>
-                            {userData.certificates.map((certificate, index) => {
-                                return (
-                                    <div key={index}
-                                         className="flex justify-left items-center whitespace-nowrap">
+                            {userData.certificates.length !== 0 &&
+                            (<>
+                                <h4 className="text-primary-500 text-left mt-10 font-bold tracking-wide">
+                                    CERTIFICATES
+                                </h4>
+                                {userData.certificates.map((certificate, index) => {
+                                    return (
+                                        <div key={index}
+                                             className="flex justify-left items-center whitespace-nowrap">
+                                            <div className="h-6 w-6">
+                                                <svg xmlns="http://www.w3.org/2000/svg"
+                                                     fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                                                     stroke="currentColor"
+                                                     className="w-6 h-6 inline-flex dark:text-white">
+                                                    <path strokeLinecap="round" strokeLinejoin="round"
+                                                          d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5"/>
+                                                </svg>
+                                            </div>
+                                            <span
+                                                className="pl-1 text-gray-600 dark:text-white text-left mt-2 font-light tracking-tighter">
+                                            {certificate}
+                                        </span>
+                                        </div>
+                                    )
+                                })}
+                            </>)
+                            }
+                            {userData.hometown && (
+                                <>
+                                    <h4 className="text-primary-500 text-left mt-4 font-bold tracking-wide">
+                                        HOMETOWN
+                                    </h4>
+                                    <div className="flex justify-left items-center whitespace-nowrap">
                                         <div className="h-6 w-6">
-                                            <svg xmlns="http://www.w3.org/2000/svg"
-                                                 fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
-                                                 className="w-6 h-6 inline-flex dark:text-white">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                 strokeWidth={1.5} stroke="currentColor"
+                                                 className="inline-flex w-6 h-6 dark:text-white">
                                                 <path strokeLinecap="round" strokeLinejoin="round"
-                                                      d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5"/>
+                                                      d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"/>
                                             </svg>
                                         </div>
                                         <span
                                             className="pl-1 text-gray-600 dark:text-white text-left mt-2 font-light tracking-tighter">
-                                            {certificate}
-                                        </span>
-                                    </div>
-                                )
-                            })}
-                            <h4 className="text-primary-500 text-left mt-4 font-bold tracking-wide">
-                                HOMETOWN
-                            </h4>
-                            <div className="flex justify-left items-center whitespace-nowrap">
-                                <div className="h-6 w-6">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                         strokeWidth={1.5} stroke="currentColor"
-                                         className="inline-flex w-6 h-6 dark:text-white">
-                                        <path strokeLinecap="round" strokeLinejoin="round"
-                                              d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"/>
-                                    </svg>
-                                </div>
-                                <span
-                                    className="pl-1 text-gray-600 dark:text-white text-left mt-2 font-light tracking-tighter">
-                                  {userData.hometown}
+                            {userData.hometown}
                                 </span>
-                            </div>
+                                    </div>
+                                </>
+                            )
+                            }
                         </div>
 
                         <div
