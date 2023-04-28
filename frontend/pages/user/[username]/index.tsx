dd904d84a78f42694b7493bd9da77a100212144b
benchanjamin <47726262+benchanjamin@users.noreply.github.com>
2023-04-28 17:47:23 -0400

    ordering change of grid items

--- frontend/pages/user/[username]/index.tsx
+++ frontend/pages/user/[username]/index.tsx
@@ -176,29 +176,8 @@ const Index: React.FC<Props> = ({userData, headerData}) => {
                         </div>
 
                         <div
-                        className="flex flex-col items-center sm:items-start order-last mb-4 sm:order-3 sm:mb-0">
-                            {userData.housing && (
-                                <>
-                                    <h4 className="text-primary-500 text-left mt-4 font-bold tracking-wide">
-                                        HOUSING
-                                    </h4>
-                                    <div className="flex justify-left items-center whitespace-nowrap">
-                                        <div className="h-6 w-6">
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                 strokeWidth={1.5} stroke="currentColor"
-                                                 className="inline-flex w-6 h-6 dark:text-white">
-                                                <path strokeLinecap="round" strokeLinejoin="round"
-                                                      d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"/>
-                                            </svg>
-                                        </div>
-                                        <span
-                                            className="pl-1 text-gray-600 dark:text-white text-left mt-2 font-light tracking-tighter">
-                            {userData.housing}
-                                </span>
-                                    </div>
-                                </>
-                            )
-                            }
+                        className="flex flex-col items-center sm:items-start order-last mb-4 sm:order-4 sm:mb-0">
+
                             {userData.interests?.length !== 0 &&
                                 (<>
                                     <h4 className="text-primary-500 text-left mt-4 sm:mt-0 sm:mt-3 font-bold tracking-wide">
@@ -253,10 +232,32 @@ const Index: React.FC<Props> = ({userData, headerData}) => {
                                     })}
                                 </>)
                             }
+                            {userData.housing && (
+                                <>
+                                    <h4 className="text-primary-500 text-left mt-4 font-bold tracking-wide">
+                                        HOUSING
+                                    </h4>
+                                    <div className="flex justify-left items-center whitespace-nowrap">
+                                        <div className="h-6 w-6">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                 strokeWidth={1.5} stroke="currentColor"
+                                                 className="inline-flex w-6 h-6 dark:text-white">
+                                                <path strokeLinecap="round" strokeLinejoin="round"
+                                                      d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"/>
+                                            </svg>
+                                        </div>
+                                        <span
+                                            className="pl-1 text-gray-600 dark:text-white text-left mt-2 font-light tracking-tighter">
+                            {userData.housing}
+                                </span>
+                                    </div>
+                                </>
+                            )
+                            }
                         </div>
 
                         <div
-                            className="flex flex-col items-center sm:items-start order-4 sm:order-0 sm:mb-0">
+                            className="flex flex-col items-center sm:items-start order-4 sm:order-3 sm:mb-0">
                             {userData.certificates.length !== 0 &&
                                 (<>
                                     <h4 className="text-primary-500 text-left mt-4 font-bold tracking-wide">
