d4120f31d6cadee0f37e85ae0139e6fc09734579
benchanjamin <47726262+benchanjamin@users.noreply.github.com>
2023-04-28 16:53:03 -0400

    add magnifying glass icon to search and list

--- frontend/pages/list.tsx
+++ frontend/pages/list.tsx
@@ -148,8 +148,10 @@ const List: React.FC<Props> = ({headerData, listData}) => {
                                                       autoComplete="off"/>
                                 </div>
                                 <button onClick={async () => await router.push(`/list/?q=${encodeURIComponent(query)}`)}
-                                        className="bg-primary-400 hover:bg-primary-500 text-white px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-primary-400 focus:ring-opacity-50">
-                                    List
+                                        className="bg-primary-400 hover:bg-primary-500 text-white px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-primary-400 focus:ring-opacity-5 mt-1.50">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 dark:text-white">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                                    </svg>
                                 </button>
                             </div>
                             <div
