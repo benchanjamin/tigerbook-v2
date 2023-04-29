84b614e096ebfc7477cbea18116b2576ccff635a
benchanjamin <47726262+benchanjamin@users.noreply.github.com>
2023-04-29 17:06:02 -0400

    update top right drop down menu

--- frontend/components/dropdown-menu/DropdownMenu.tsx
+++ frontend/components/dropdown-menu/DropdownMenu.tsx
@@ -62,7 +62,30 @@ export default function DropdownMenu({profilePicSrc, username, hasProfile}) {
                                             aria-hidden="true"
                                         />
                                     )}
-                                    Edit Profile
+                                    &nbsp;Edit Profile
+                                </div>
+                            )}
+                        </Menu.Item>}
+                        {hasProfile && <Menu.Item>
+                            {({active}) => (
+                                <div
+                                    onClick={async () => await router.push(`/profile/preview`)}
+                                    className={`${
+                                        active ? 'bg-primary-300 text-white' : 'text-gray-900'
+                                    } group flex w-full items-center rounded-md px-2 py-2 text-sm cursor-pointer`}
+                                >
+                                    {active ? (
+                                        <ProfilePreviewActiveIcon
+                                            className="mr-2 h-5 w-5 pr-2"
+                                            aria-hidden="true"
+                                        />
+                                    ) : (
+                                        <ProfilePreviewInactiveIcon
+                                            className="mr-2 h-5 w-5 pr-2"
+                                            aria-hidden="true"
+                                        />
+                                    )}
+                                    &nbsp;Preview Full Profile
                                 </div>
                             )}
                         </Menu.Item>}
@@ -85,7 +108,7 @@ export default function DropdownMenu({profilePicSrc, username, hasProfile}) {
                                             aria-hidden="true"
                                         />
                                     )}
-                                    Logout
+                                    &nbsp;Logout
                                 </Link>
                             )}
                         </Menu.Item>
@@ -146,6 +169,30 @@ function ProfileEditInactiveIcon(props) {
                   fill="#E77500"
                   stroke="#FFC68B"
                   d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"/>
+        </svg>
+    )
+}
+
+function ProfilePreviewActiveIcon(props) {
+    return (
+        <svg {...props} xmlns="http://www.w3.org/2000/svg"
+             fill="#E77500"
+             stroke="#FFC68B"
+             viewBox="0 0 24 24" strokeWidth={1.5}  className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+        </svg>
+    )
+}
+
+function ProfilePreviewInactiveIcon(props) {
+    return (
+        <svg {...props} xmlns="http://www.w3.org/2000/svg"
+             fill="#E77500"
+             stroke="#FFC68B"
+             viewBox="0 0 24 24" strokeWidth={1.5}  className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
         </svg>
     )
 }
