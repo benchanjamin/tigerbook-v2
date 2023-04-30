316145d6c684e7626a348940a76d84060818ac43
benchanjamin <47726262+benchanjamin@users.noreply.github.com>
2023-04-30 04:24:33 -0400

    update with more directory entries

--- frontend/pages/list.tsx
+++ frontend/pages/list.tsx
@@ -175,10 +175,12 @@ const List: React.FC<Props> = ({headerData}) => {
                                                       zIndex={100} setterFunction={setQuery}
                                                       autoComplete="off"
                                                       onEnterFunction={async () => {
+                                                          setPage(1)
                                                           await onEnter()
                                                       }}/>
                                 </div>
                                 <button onClick={async () => {
+                                    setPage(1)
                                     await onEnter()
                                 }}
                                         className="bg-primary-400 hover:bg-primary-500 text-white px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-primary-400 focus:ring-opacity-5 mt-1.5">
