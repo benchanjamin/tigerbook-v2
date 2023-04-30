5b838827db5db38bee7fc0473ad96da20074651d
benchanjamin <47726262+benchanjamin@users.noreply.github.com>
2023-04-30 09:16:26 -0400

    update list view ui

--- frontend/pages/list.tsx
+++ frontend/pages/list.tsx
@@ -159,9 +159,8 @@ const List: React.FC<Props> = ({headerData}) => {
         const apiListAPIRoutes = [
             '/api-django/concentrations/',
             '/api-django/tracks/',
-            '/api-django/residential-colleges/',
             '/api-django/class-years/',
-            // '/api-django/cities/',
+            '/api-django/residential-colleges/',
             '/api-django/certificates/',
             '/api-django/pronouns/',
             '/api-django/interests/',
@@ -169,36 +168,37 @@ const List: React.FC<Props> = ({headerData}) => {
             '/api-django/extracurricular-positions/',
             '/api-django/housing/',
             '/api-django/research-types/'
+            // '/api-django/cities/',
         ]
 
         const keys = [
             'concentrations',
             'tracks',
-            'residentialColleges',
             'classYears',
-            // 'cities',
+            'residentialColleges',
             'certificates',
             'pronouns',
             'interests',
             'extracurriculars',
             'positions',
             'completeHousing',
-            'researchTypes'
+            'researchTypes',
+            // 'cities',
         ]
 
         const indices = [
             'concentration',
             'track',
-            'residential_college',
             'class_year',
-            // 'complete_city',
+            'residential_college',
             'certificate',
             'pronouns',
             'interest',
             'extracurricular',
             'position',
             'complete_housing',
-            'research_type'
+            'research_type',
+            // 'complete_city',
         ]
 
         const listData: ListData = {}
@@ -351,6 +351,75 @@ const List: React.FC<Props> = ({headerData}) => {
                                             </svg>
                                             <span
                                                 className="flex-1 ml-3 text-left whitespace-nowrap">Basic Filters</span>
+                                            <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
+                                                 xmlns="http://www.w3.org/2000/svg">
+                                                <path fillRule="evenodd"
+                                                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                      clipRule="evenodd"></path>
+                                            </svg>
+                                        </button>
+                                        <ul id="dropdown-example" className="hidden">
+                                            <li>
+                                                <label htmlFor="concentrations"
+                                                       className="block my-1 text-sm font-medium text-gray-900 dark:text-white pl-1">Concentrations</label>
+                                                <TigerBookComboBoxMultipleStrictSelect
+                                                    data={concentrationsList}
+                                                    defaultText="Select concentrations"
+                                                    initialSelected={[]}
+                                                    zIndex={50}
+                                                    setterFunction={setConcentrationQuery}
+                                                />
+                                            </li>
+                                            <li>
+                                                <label htmlFor="tracks"
+                                                       className="block my-1 text-sm font-medium text-gray-900 dark:text-white pl-1">Tracks</label>
+                                                <TigerBookComboBoxMultipleStrictSelect
+                                                    data={tracksList}
+                                                    defaultText="Select tracks"
+                                                    initialSelected={[]}
+                                                    zIndex={40}
+                                                    setterFunction={setTracksQuery}
+                                                />
+                                            </li>
+                                            <li>
+                                                <label htmlFor="class-years"
+                                                       className="block my-1 text-sm font-medium text-gray-900 dark:text-white pl-1">Class Years</label>
+                                                <TigerBookComboBoxMultipleStrictSelect
+                                                    data={classYearsList}
+                                                    defaultText="Select class years"
+                                                    initialSelected={[]}
+                                                    zIndex={31}
+                                                    setterFunction={setClassYearsQuery}
+                                                />
+                                            </li>
+                                            <li>
+                                                <label htmlFor="residential-colleges"
+                                                       className="block my-1 text-sm font-medium text-gray-900 dark:text-white pl-1">Residential Colleges</label>
+                                                <TigerBookComboBoxMultipleStrictSelect
+                                                    data={concentrationsList}
+                                                    defaultText="Select residential colleges"
+                                                    initialSelected={[]}
+                                                    zIndex={30}
+                                                    setterFunction={setResCollegeQuery}
+                                                />
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <button type="button"
+                                                className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                                                aria-controls="dropdown-example"
+                                                data-collapse-toggle="dropdown-example">
+                                            <svg aria-hidden="true"
+                                                 className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+                                                 fill="currentColor" viewBox="0 0 20 20"
+                                                 xmlns="http://www.w3.org/2000/svg">
+                                                <path fillRule="evenodd"
+                                                      d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z"
+                                                      clipRule="evenodd"></path>
+                                            </svg>
+                                            <span
+                                                className="flex-1 ml-3 text-left whitespace-nowrap">Advanced Filters</span>
                                             <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
                                                  xmlns="http://www.w3.org/2000/svg">
                                                 <path fillRule="evenodd"
