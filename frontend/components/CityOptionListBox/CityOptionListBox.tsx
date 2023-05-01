ffa676ea59915c6697ef44778bea0903f6b26a56
benchanjamin <47726262+benchanjamin@users.noreply.github.com>
2023-05-01 10:03:46 -0400

    test map

--- frontend/components/CityOptionListBox/CityOptionListBox.tsx
+++ frontend/components/CityOptionListBox/CityOptionListBox.tsx
@@ -40,7 +40,7 @@ export default function CityOptionListBox({onChange}) {
                                         key={optionIdx}
                                         className={({active}) =>
                                             `relative cursor-pointer select-none py-2 pl-10 pr-4 ${
-                                                active ? 'bg-darkBlue text-white' : 'text-gray-900'
+                                                active ? 'bg-primary-500 text-white' : 'text-gray-900'
                                             }`
                                         }
                                         value={option}
@@ -57,7 +57,7 @@ export default function CityOptionListBox({onChange}) {
                                                 {selected ? (
                                                     <span
                                                         className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
-                                                            active ? 'text-white' : 'text-darkBlue'
+                                                            active ? 'text-white' : 'text-primary-500'
                                                         }`}>
                           <CheckIcon className="h-5 w-5" aria-hidden="true"/>
                         </span>
