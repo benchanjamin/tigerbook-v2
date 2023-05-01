3445b910dc8c8e4949fee95aab6a6037dd0a4bdf
benchanjamin <47726262+benchanjamin@users.noreply.github.com>
2023-05-01 03:16:34 -0400

    fix bug in multiselect

--- frontend/components/headless-ui/TigerBookComboBoxMultipleStrictSelect.tsx
+++ frontend/components/headless-ui/TigerBookComboBoxMultipleStrictSelect.tsx
@@ -41,7 +41,8 @@ export default function TigerBookComboBoxMultipleStrictSelect(
 
         // @ts-ignore
         const associatedText = event.target.parentNode.innerText ??
-            event.target.parentNode.parentNode.innerText        console.log("actual target" , event.target)
+            event.target.parentNode.parentNode.innerText
+        console.log("actual target" , event.target)
         console.log("associated text" , associatedText)
         setSelected(prevArray => {
             let result = prevArray.filter(item => String(item) !== String(associatedText))
