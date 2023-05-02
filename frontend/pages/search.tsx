f9afb92ae3ff022a0faa1f8a08ff2e297e15c742
benchanjamin <47726262+benchanjamin@users.noreply.github.com>
2023-05-02 13:15:10 -0400

    test controllers in search.tsx

--- frontend/pages/search.tsx
+++ frontend/pages/search.tsx
@@ -562,7 +562,7 @@ const Search: React.FC<Props> = ({headerData}) => {
             setIsLoading(true)
             const axios = await axiosInstance();
             let listURL = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api-django/list/?page=${page}`;
-            listURL += `${firstQuery}${additionalQueries}`
+            listURL += `q=${firstQuery}${additionalQueries}`
             console.log('SearchURL', listURL)
             const axiosResponse = await axios.get(listURL, {
                 signal: onLoadController.signal
